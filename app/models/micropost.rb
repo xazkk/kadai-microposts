2c46c5e8f34ede3e4244593bc28c601e749ab494
kenji.komachi <rgby1308xazkk@gmail.com>
2020-03-28 13:05:44 +0000

    favorite model&contorller

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,4 +2,5 @@ class Micropost < ApplicationRecord
     belongs_to :user
     
     validates :content, presence: true, length: { maximum: 255 }
+    has_many :favorites
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,9 @@ class User < ApplicationRecord
     has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
     has_many :followers, through: :reverses_of_relationship, source: :user
     
+    has_many :favorites
+    has_many :likes, through: :favorites, source: :micropost
+    
     def follow(other_user)
       unless self == other_user
       self.relationships.find_or_create_by(follow_id: other_user.id)
@@ -25,6 +28,19 @@ class User < ApplicationRecord
 
     def following?(other_user)
       self.followings.include?(other_user)
+    end
+    
+    def favorite(fav_micropost)
+      self.favorites.find_or_create_by(micropost_id: fav_micropost.id)
+    end
+    
+    def unfavorite(fav_micropost)
+      fav_post = self.favorites.find_by(micropost_id: fav_micropost.id)
+      fav_post.destroy if favpost
+    end  
+    
+    def liked?(fav_micropost)
+      self.likes.include?(fav_micropost)
     end
     
     def feed_microposts
